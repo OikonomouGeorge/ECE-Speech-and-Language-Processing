0d0b423473241213e2297c1e369442da1d60c125
zeno <igeorgeoikonomou@gmail.com>
2025-03-27 21:25:16 +0200

    Update kaldi_performance.sh

--- Lab 1/kaldi_performance.sh
+++ Lab 1/kaldi_performance.sh
@@ -1,1 +1,13 @@
+. ./path.sh 
 
+cd "data/local/lm_tmp"
+export IRSTLM=$KALDI_ROOT/tools/irstlm/
+export PATH=${PATH}:$IRSTLM/bin
+
+compile-lm lm_test1.ilm.gz --eval=../dict/lm_dev.text 
+compile-lm lm_test2.ilm.gz --eval=../dict/lm_dev.text 
+
+compile-lm lm_dev1.ilm.gz --eval=../dict/lm_test.text
+compile-lm lm_dev2.ilm.gz --eval=../dict/lm_test.text
+
+cd "../../../"
