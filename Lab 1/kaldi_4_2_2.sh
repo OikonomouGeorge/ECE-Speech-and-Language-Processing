b2ac456f991bd138e11f581b09cf74a5030c5600
zeno <igeorgeoikonomou@gmail.com>
2025-03-27 14:27:37 +0200

    Create kaldi_4_2_2.sh

---
+++ Lab 1/kaldi_4_2_2.sh
@@ -0,0 +1,22 @@
+. ./path.sh
+
+export IRSTLM=$KALDI_ROOT/tools/irstlm/
+export PATH=${PATH}:$IRSTLM/bin
+
+cd "/home/zeno/kaldi/egs/project1/data/local/dict"
+
+build-lm.sh -i "lm_train.text" -n 1 -o lm_train1.ilm.gz
+build-lm.sh -i "lm_train.text" -n 2 -o lm_train2.ilm.gz
+build-lm.sh -i "lm_test.text" -n 1 -o lm_test1.ilm.gz
+build-lm.sh -i "lm_test.text" -n 2 -o lm_test2.ilm.gz
+build-lm.sh -i "lm_dev.text" -n 1 -o lm_dev1.ilm.gz
+build-lm.sh -i "lm_dev.text" -n 2 -o lm_dev2.ilm.gz
+
+mv lm_train1.ilm.gz ../lm_tmp
+mv lm_train2.ilm.gz ../lm_tmp
+mv lm_test1.ilm.gz ../lm_tmp
+mv lm_test2.ilm.gz ../lm_tmp
+mv lm_dev1.ilm.gz ../lm_tmp
+mv lm_dev2.ilm.gz ../lm_tmp
+
+cd "../../../"
