37c25871c4ee9f9b01555bd6577270e2ea524d47
zeno <igeorgeoikonomou@gmail.com>
2025-03-28 11:41:20 +0200

    Update kaldi_4_4_1.sh

--- Lab 1/kaldi_4_4_1.sh
+++ Lab 1/kaldi_4_4_1.sh
@@ -5,11 +5,7 @@ cd /home/zeno/kaldi/egs/wsj/s5
 data_dir="/home/zeno/kaldi/egs/project1/data"
 
 /home/zeno/kaldi/egs/wsj/s5/steps/train_mono.sh --nj $(nproc) --cmd run.pl \
-    $data_dir/train $data_dir/lang $data_dir/kaldi_4_4 || exit 1;
+    $data_dir/train $data_dir/lang $data_dir/kaldi_monophone || exit 1;
 
-
-
-#steps/train_mono.sh --boost-silence 1.25 --nj 4 --cmd "$train_cmd" \
-#  data/train data/lang_test exp/mono0a || exit 1;
-#steps/align_si.sh --boost-silence 1.25 --nj 4 --cmd "$train_cmd" \
-#  data/train data/lang_test exp/mono0a exp/mono0a_ali
+/home/zeno/kaldi/egs/wsj/s5/steps/lign_si.sh --nj $(nproc) --cmd run.pl \
+    $data_dir/train $data_dir/lang $data_dir/kaldi_monophone $data_dir/kaldi_monophone
