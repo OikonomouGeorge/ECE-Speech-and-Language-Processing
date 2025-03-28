ea35b61c212693a54e9c3e8cc79a6ca14e373905
zeno <igeorgeoikonomou@gmail.com>
2025-03-28 15:15:50 +0200

    Update kaldi_4_4_5.sh

--- Lab 1/kaldi_4_4_5.sh
+++ Lab 1/kaldi_4_4_5.sh
@@ -12,6 +12,12 @@ data_dir="/home/zeno/kaldi/egs/project1/data"
 /home/zeno/kaldi/egs/wsj/s5/steps/train_deltas.sh --cmd "$train_cmd" 2500 15000 \
   $data_dir/train $data_dir/lang $data_dir/kaldi_monophone/mono_ali $data_dir/kaldi_triphone || exit 1;
 
-    # Trigram Graph Creation
+# Trigram Graph Creation
 /home/zeno/kaldi/egs/wsj/s5/utils/mkgraph.sh $data_dir/lang \
   $data_dir/kaldi_triphone $data_dir/kaldi_triphone/graph/trigram || exit 1;
+
+# Usage: steps/decode.sh [options] <graph-dir> <data-dir> <decode-dir>
+# Unigram Dev
+/home/zeno/kaldi/egs/wsj/s5/steps/decode.sh $data_dir/kaldi_triphone/graph/trigram $data_dir/dev $data_dir/kaldi_triphone/graph/trigram/decode_dev || exit 1;
+# Unigram Test
+/home/zeno/kaldi/egs/wsj/s5/steps/decode.sh $data_dir/kaldi_triphone/graph/trigram $data_dir/test $data_dir/kaldi_triphone/graph/trigram/decode_test|| exit 1;
