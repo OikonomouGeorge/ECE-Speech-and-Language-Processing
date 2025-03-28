282d68058bda60ecb848ca65c4783445601fa943
zeno <igeorgeoikonomou@gmail.com>
2025-03-28 11:38:49 +0200

    Update kaldi_4_4_1.sh

--- Lab 1/kaldi_4_4_1.sh
+++ Lab 1/kaldi_4_4_1.sh
@@ -4,7 +4,12 @@
 cd /home/zeno/kaldi/egs/wsj/s5
 data_dir="/home/zeno/kaldi/egs/project1/data"
 
-for x in train test dev; do
-    /home/zeno/kaldi/egs/wsj/s5/steps/make_mfcc.sh --mfcc-config /home/zeno/kaldi/egs/project1/data/conf/mfcc.conf --nj $(nproc) --cmd run.pl $data_dir/$x $data_dir/$x/logs $data_dir/$x/mfcc || exit 1;
-    /home/zeno/kaldi/egs/wsj/s5/steps/compute_cmvn_stats.sh $data_dir/$x $data_dir/$x/logs $data_dir/$x/mfcc || exit 1;
-done
+/home/zeno/kaldi/egs/wsj/s5/steps/train_mono.sh --nj $(nproc) --cmd run.pl \
+    $data_dir/train $data_dir/lang $data_dir/kaldi_4_4 || exit 1;
+
+
+
+#steps/train_mono.sh --boost-silence 1.25 --nj 4 --cmd "$train_cmd" \
+#  data/train data/lang_test exp/mono0a || exit 1;
+#steps/align_si.sh --boost-silence 1.25 --nj 4 --cmd "$train_cmd" \
+#  data/train data/lang_test exp/mono0a exp/mono0a_ali
