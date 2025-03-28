712f850f28cdd9283ef6dac3a53b1e2dc166029d
zeno <igeorgeoikonomou@gmail.com>
2025-03-28 15:06:28 +0200

    Update kaldi_4_4_5.sh

--- Lab 1/kaldi_4_4_5.sh
+++ Lab 1/kaldi_4_4_5.sh
@@ -4,6 +4,10 @@
 cd /home/zeno/kaldi/egs/wsj/s5
 data_dir="/home/zeno/kaldi/egs/project1/data"
 
-# Alignment
-steps/align_si.sh --cmd "$train_cmd" \
-  $data_dir/train $lang_dir $exp_dir/mono || exit 1;
+# Alignment Usage: steps/align_si.sh <data-dir> <lang-dir> <src-dir> <align-dir>
+/home/zeno/kaldi/egs/wsj/s5/steps/align_si.sh --cmd "$train_cmd" \ 
+  $data_dir/train $data_dir/lang $data_dir/kaldi_monophone $data_dir/kaldi_monophone || exit 1;
+
+# Train Deltas Usage: steps/train_deltas.sh <num-leaves> <tot-gauss> <data-dir> <lang-dir> <alignment-dir> <exp-dir>
+/home/zeno/kaldi/egs/wsj/s5/steps/train_deltas.sh --cmd "$train_cmd" 2500 15000 \
+  $data_dir/train $data_dir/lang $data_dir/kaldi_monophone/mono_ali $data_dir/kaldi_triphone || exit 1;
