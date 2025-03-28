caf9d4cb8cec1c2788c46c76eb8ca83b8f8cab32
zeno <igeorgeoikonomou@gmail.com>
2025-03-28 14:42:20 +0200

    Update kaldi_4_4_5.sh

--- Lab 1/kaldi_4_4_5.sh
+++ Lab 1/kaldi_4_4_5.sh
@@ -1,3 +1,9 @@
 . ./path.sh
 . ./cmd.sh
 
+cd /home/zeno/kaldi/egs/wsj/s5
+data_dir="/home/zeno/kaldi/egs/project1/data"
+
+# Alignment
+steps/align_si.sh --cmd "$train_cmd" \
+  $data_dir/train $lang_dir $exp_dir/mono || exit 1;
